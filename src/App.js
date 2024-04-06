0cbb975564ad4b81c18bbfa09a23f9733c78c8cc
Hyerin Sung <hyerinx@naver.com>
2024-04-06 21:52:10 +0900

    데이터 스키마

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import Header from "./Header";
 import TodoList from "./TodoList";
 import TodoAdd from "./TodoAdd"
 import './App.css';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    {
+      id: 1,
+      text: "첫 번째 투두",
+      done: false,
+    },
+    {
+      id: 2,
+      text: "두 번째 투두",
+      done: false,
+    }
+  ]);
   return (
     <div className="App">
       <Header />
