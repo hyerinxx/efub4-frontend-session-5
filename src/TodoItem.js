f4eb4cbcaee7155e549899d5fc0c3216cb5db6a6
Hyerin Sung <hyerinx@naver.com>
2024-04-07 17:05:25 +0900

    todo 완료 기능

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,11 +3,19 @@ function TodoItem({ todoThis, todoList, setTodoList }) {
         setTodoList(todoList.filter(todoItem => todoItem.id !== todoThis.id));
     }
 
+    function completeTodo() {
+        setTodoList(
+            todoList.map((todoItem) => {
+                return todoItem.id === todoThis.id ? { ...todoThis, done: !todoThis.done } : todoItem;
+            })
+        );
+    }
+
     return(
         <li className="todo-item">
-            <input type="checkbox" checked={todoThis.done} />
+            <input type="checkbox" checked={todoThis.done} onClick={completeTodo} />
             <span>{todoThis.text}</span>
-            <input type="button" value="X" onClick={deleteTodo}/>
+            <input type="button" value="X" onClick={deleteTodo} />
         </li>
     )
 }
