640c795b28b6f5a167d271ea7aa177537834190a
Hyerin Sung <hyerinx@naver.com>
2024-04-07 18:10:39 +0900

    todo 저장 기능

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./Header";
 import TodoList from "./TodoList";
 import TodoAdd from "./TodoAdd"
 import './App.css';
 
+const TODO_LIST_KEY = "todoList";
+
 function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState(() => {
+    const savedTodoList = localStorage.getItem(TODO_LIST_KEY);
+    return savedTodoList ? JSON.parse(savedTodoList) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(TODO_LIST_KEY, JSON.stringify(todoList));
+  }, [todoList]);
+
   return (
     <div className="App">
       <Header />
